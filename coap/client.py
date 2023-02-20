4de5de20c705d8257c954da5fc3ac1840dcdfe0c
Stepan Kalinin <skalini@ncsu.edu>
2023-02-19 21:09:06 -0500

    Add Python code for coap

---
+++ coap/client.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import aiocoap
+import argparse
+import asyncio
+import logging
+import socket
+import time
+
+import numpy as np
+
+import aiocoap.transports.udp6
+
+class CountingSocket(socket.socket):
+    bytes_received = 0
+
+    def recvmsg(self, *args, **kwargs):
+        ret = super().recvmsg(*args, **kwargs)
+        self.increase_recv_counter(len(ret[0]))
+        return ret
+
+    @classmethod
+    def reset_recv_counter(cls):
+        cls.bytes_received = 0
+
+    @classmethod
+    def increase_recv_counter(cls, count):
+        cls.bytes_received += count
+
+    @classmethod
+    def get_recv_counter(cls):
+        return cls.bytes_received
+
+
+@classmethod
+async def new_create_client_transport_endpoint(cls, ctx, log, loop):
+    sock = CountingSocket(family=socket.AF_INET6, type=socket.SOCK_DGRAM)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+
+    return await cls._create_transport_endpoint(sock, ctx, log, loop)
+
+# Monkeypatching aiocoap to use our counting sockets
+aiocoap.transports.udp6.MessageInterfaceUDP6.create_client_transport_endpoint = new_create_client_transport_endpoint
+
+
+async def measure_download_time(protocol, uri):
+    CountingSocket.reset_recv_counter()
+    start_time = time.perf_counter_ns()
+    msg = aiocoap.Message(code=aiocoap.GET, uri=uri)
+    response = await protocol.request(msg).response
+    end_time = time.perf_counter_ns()
+    return end_time - start_time, CountingSocket.get_recv_counter()
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="name of the file to get")
+    parser.add_argument("-c", "--count", type=int, default=1, help="number of times the requested file should be downloaded")
+    parser.add_argument("--host", default="localhost", help="host to connect to")
+    parser.add_argument("--port", default="5683", help="port to connect to")
+    parser.add_argument("--expected-size", type=int, default=None, help="expected size for calculating overhead")
+
+    args = parser.parse_args()
+
+    protocol = await aiocoap.Context.create_client_context()
+    uri = f"coap://{args.host}:{args.port}/hw_files/{args.file}"
+
+    data = [await measure_download_time(protocol, uri) for _ in range(args.count)]
+
+    times, recv = list(zip(*data))
+    times = np.array(times)
+    recv = np.array(recv)
+
+    print(f"Receive time:   {np.mean(times)} ± {np.std(times)} ns")
+    print(f"Bytes received: {np.mean(recv)} ± {np.std(recv)} bytes")
+    if args.expected_size is not None:
+        print(f"Overhead:       {np.mean(recv - args.expected_size)} ± {np.std(recv - args.expected_size)} bytes")
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
---
+++ coap/server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+
+from plumbum import local
+
+import asyncio
+
+import aiocoap
+import aiocoap.error as error
+import aiocoap.resource as resource
+
+
+class NoSuchFile(error.NotFound): # just for the better error msg
+    message = "Error: File not found!"
+
+
+class FileResource(resource.Resource, resource.PathCapable):
+    def __init__(self, root):
+        super().__init__()
+        self.root = local.path(root)
+
+    def request_to_localpath(self, request):
+        path = request.opt.uri_path
+        if any('/' in p or p in ('.', '..') for p in path):
+            raise InvalidPathError()
+
+        return self.root / "/".join(path)
+
+    async def render_get(self, request):
+        p = self.request_to_localpath(request)
+        if not p.is_file():
+            raise NoSuchFile
+
+        with open(p, "rb") as f:
+            data = f.read()
+        return aiocoap.Message(
+                payload=data,
+                content_format=aiocoap.numbers.ContentFormat.OCTETSTREAM
+                )
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root", help="directory to serve files from")
+
+    args = parser.parse_args()
+
+    root = resource.Site()
+    root.add_resource([".well-known", "core"],
+                      resource.WKCResource(root.get_resources_as_linkheader))
+    root.add_resource(["hw_files"], FileResource(args.root))
+    
+    await aiocoap.Context.create_server_context(root)
+
+    await asyncio.get_running_loop().create_future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
